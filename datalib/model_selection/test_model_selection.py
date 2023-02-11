4636b4f572b2c415414448fc1665d461c81db9d0
Carlo <carlo_lemos@hotmail.com>
2023-02-11 12:48:51 -0300

    update lint with black

--- datalib/model_selection/test_model_selection.py
+++ datalib/model_selection/test_model_selection.py
@@ -1,3 +1,16 @@
 # TODO: remove
 def test_example():
     assert 1 == 1
+
+def test_nilvo():
+    assert 1 == 1
+
+    # long line
+    variable = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+    
+    # long line
+    variable = 1 +1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+
+    variable1 = variable
+
+    assert variable==variable
