35b94d9a6d63245530a6f1f9b6735f22e78ae446
Carlo <carlo_lemos@hotmail.com>
2023-02-11 12:55:08 -0300

    removing black testing file

--- datalib/model_selection/test_model_selection.py
+++ datalib/model_selection/test_model_selection.py
@@ -1,16 +1,3 @@
 # TODO: remove
 def test_example():
     assert 1 == 1
-
-def test_nilvo():
-    assert 1 == 1
-
-    # long line
-    variable = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
-    
-    # long line
-    variable = 1 +1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
-
-    variable1 = variable
-
-    assert variable==variable
