d26926e562ceab1046145fe463306388d03c006b
Jean C Maia <jeanclmaia@gmail.com>
2023-02-14 18:58:50 -0300

    A jaggernaut code refactor with all code suggestions

--- datalib/tests/test_ranking.py
+++ datalib/tests/test_ranking.py
@@ -1,33 +1,14 @@
-from ..metrics import delinquency_curve, DeliquencyDisplay
-from numpy.testing import assert_array_equal, assert_almost_equal, assert_allclose
-from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
-
 import numpy as np
 import pytest
 
-
-def test_delinquency_curve__success_case():
-
-    y_true = np.array([0, 0, 1, 1])
-    y_scores = np.array([0.1, 0.4, 0.3, 0.8])
-    approval_rate, default_rate, optimal_rate = delinquency_curve(y_true, y_scores)
-
-    assert_array_equal(approval_rate, np.array([0, 0.25, 0.5, 0.75, 1.0]))
-    assert_almost_equal(default_rate, np.array([0, 0, 0.5, 0.3333333, 0.5]), decimal=5)
-    assert_almost_equal(optimal_rate, np.array([0, 0, 0, 0.3333333, 0.5]), decimal=5)
-
-
-def test_delinquency_curve__multilabel_excpetion():
-
-    y_true = np.array([0, 0, 1, 1, 2])
-    y_scores = np.array([0.1, 0.4, 0.3, 0.8, 0.04])
-    with pytest.raises(Exception) as exc_info:
-        _, _ = delinquency_curve(y_true, y_scores)
-    assert (
-        str(exc_info.value)
-        == "Only binary classification is supported. Provided labels [0 1 2]."
-    )
+from ..metrics import delinquency_curve, DeliquencyDisplay
+from numpy.testing import (
+    assert_array_equal,
+    assert_almost_equal,
+    assert_allclose,
+)
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
 
 
 @pytest.fixture(scope="module")
@@ -41,7 +22,47 @@ def iris_data_binary(iris_data):
     return X[y < 2], y[y < 2]
 
 
-def test_calibration_display_compute(iris_data_binary):
+def test_deliquency_curve__initial_and_end_values():
+
+    y_true = np.array([0, 0, 1, 1])
+    y_scores = np.array([0.1, 0.4, 0.3, 0.8])
+    approval_rate, _, _ = delinquency_curve(y_true, y_scores)
+
+    assert approval_rate[0] == 0
+    assert approval_rate[-1] == 1
+
+
+def test_delinquency_curve__success_case():
+
+    y_true = np.array([0, 0, 1, 1])
+    y_scores = np.array([0.1, 0.4, 0.3, 0.8])
+    approval_rate, default_rate, optimal_rate = delinquency_curve(
+        y_true, y_scores
+    )
+
+    assert_array_equal(approval_rate, np.array([0, 0.25, 0.5, 0.75, 1.0]))
+    assert_almost_equal(
+        default_rate, np.array([0, 0, 0.5, 0.3333333, 0.5]), decimal=5
+    )
+    assert_almost_equal(
+        optimal_rate, np.array([0, 0, 0, 0.3333333, 0.5]), decimal=5
+    )
+
+
+def test_delinquency_curve__multilabel_exception():
+
+    y_true = np.array([0, 0, 1, 1, 2])
+    y_scores = np.array([0.1, 0.4, 0.3, 0.8, 0.04])
+    with pytest.raises(Exception) as exc_info:
+        _, _ = delinquency_curve(y_true, y_scores)
+    assert (
+        str(exc_info.value)
+        == "Only binary classification is supported. Provided labels [0 1 2]."
+    )
+
+
+def test_deliquency_display__assess_plot_parameters(iris_data_binary):
+
     X, y = iris_data_binary
 
     lr = LogisticRegression().fit(X, y)
