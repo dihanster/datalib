8e930460ac1b6c81ba90b6a7ed86616b97aad269
Willian Dihanster <wdihanster@gmail.com>
2023-03-03 10:55:23 -0300

    General linting using Black and Flake8

--- datalib/metrics/_plot/cap_curve.py
+++ datalib/metrics/_plot/cap_curve.py
@@ -225,8 +225,7 @@ class CAPCurveDisplay:
         >>> from sklearn.svm import SVC
         >>> X, y = make_classification(random_state=0)
         >>> X_train, X_test, y_train, y_test = train_test_split(
-        >>>     X, y, random_state=0
-        >>> )
+        ...     X, y, random_state=0)
         >>> clf = SVC(random_state=0).fit(X_train, y_train)
         >>> CAPCurveDisplay.from_estimator(clf, X_test, y_test)
         >>> plt.show()
@@ -321,8 +320,7 @@ class CAPCurveDisplay:
         >>> from sklearn.svm import SVC
         >>> X, y = make_classification(random_state=0)
         >>> X_train, X_test, y_train, y_test = train_test_split(
-        >>>     X, y, random_state=0
-        >>> )
+        ...     X, y, random_state=0)
         >>> clf = SVC(random_state=0, probability=True).fit(X_train, y_train)
         >>> y_pred = clf.predict_proba(X_test)[:, 1]
         >>> CAPCurveDisplay.from_predictions(y_test, y_pred)
