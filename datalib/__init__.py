3b8ed2001d6542b9baedb7447b181b3f01487c52
Willian Dihanster <wdihanster@gmail.com>
2023-07-07 10:23:53 -0300

    Removed useless files (blank or useless __init__.py)

--- datalib/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import metrics
-
-__all__ = [metrics]
--- datalib/metrics/__init__.py
+++ datalib/metrics/__init__.py
@@ -1,4 +1,9 @@
 from ._ranking import cap_curve, delinquency_curve
 from ._plot import CAPCurveDisplay, DeliquencyDisplay
 
-__all__ = [cap_curve, CAPCurveDisplay, delinquency_curve, DeliquencyDisplay]
+__all__ = [
+    "cap_curve", 
+    "CAPCurveDisplay", 
+    "delinquency_curve", 
+    "DeliquencyDisplay"
+]
