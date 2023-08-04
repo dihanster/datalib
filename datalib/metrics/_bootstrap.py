8995897a92d2c7c4fe018127cf5fbbbbd64cab64
vitaliset <carlo_lemos@hotmail.com>
2023-08-04 17:33:02 -0300

    fixing sklearn regression and linting

--- datalib/metrics/_bootstrap.py
+++ datalib/metrics/_bootstrap.py
@@ -18,7 +18,7 @@ def bootstrap_metric(metric, *args, random_state=42, n_bootstrap=20, **kwargs):
         raise ValueError(
             "All the elements to be bootstraped (*args and **kwargs) "
             "are not equal in length."
-            )
+        )
 
     size = len((list(kwargs.values()) + args)[0])
 
