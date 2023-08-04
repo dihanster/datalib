d72c0e019f107e84d51181747249a40b99a08c37
vitaliset <carlo_lemos@hotmail.com>
2023-08-04 18:20:29 -0300

    covarage

--- datalib/utils/tests/test_validation.py
+++ datalib/utils/tests/test_validation.py
@@ -4,5 +4,9 @@ from datalib.utils import all_equal_elements
 def test__all_equal_elements():
     all_equal_elements_list = 10 * [0]
     not_all_equal_elements_list = 10 * [0] + 5 * [1]
+    unique_element_list = [0]
+    empty_list = []
     assert all_equal_elements(all_equal_elements_list)
     assert not all_equal_elements(not_all_equal_elements_list)
+    assert all_equal_elements(unique_element_list)
+    assert all_equal_elements(empty_list)
